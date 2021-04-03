4c2ca0caacffe83f006689dd0459af836ddd479b
DevankLikesCodingStuff <77108500+DevankLikesCodingStuff@users.noreply.github.com>
2021-04-03 11:57:01 +0530

    Delete guess_the_word_game directory

--- guess_the_word_game/game_login.js
+++
@@ -1,7 +0,0 @@
-function SumbitBtn(){
-    var player1 = document.getElementById("player1_name").value;
-    var player2 = document.getElementById("player2_name").value;
-    localStorage.setItem("SaveData1", player1);
-    localStorage.setItem("SaveData2",player2);
-    window.location="game_page.html"
-}
