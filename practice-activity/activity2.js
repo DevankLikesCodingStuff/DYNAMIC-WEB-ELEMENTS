6ddd184e60a4e217d8189d236c644b0115d5a6b8
DevankLikesCodingStuff <77108500+DevankLikesCodingStuff@users.noreply.github.com>
2021-04-03 11:56:54 +0530

    Delete practice-activity directory

--- practice-activity/activity2.js
+++
@@ -1,7 +0,0 @@
-function GetScore(){
-    var Score = localStorage.getItem("score")
-    document.getElementById("score").innerHTML= "Score = " + Score;
-}
-function BackPage(){
-    window.location = "activity_1.html"
-}
